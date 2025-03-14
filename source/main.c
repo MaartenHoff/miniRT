31b564f3de89a980e5e66e6532b00296626ce230
Maarten Hoff <maahoff@c4a7c6.42berlin.de>
2025-03-14 15:54:53 +0100

    some stuff

--- source/main.c
+++ source/main.c
@@ -6,7 +6,7 @@
 /*   By: maahoff <maahoff@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/16 18:42:02 by maahoff           #+#    #+#             */
-/*   Updated: 2025/03/14 15:42:08 by maahoff          ###   ########.fr       */
+/*   Updated: 2025/03/14 15:50:06 by maahoff          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,8 @@ int	init_window(t_window *window)
 	if (!window->win)
 		return (1);
 	window->img = mlx_new_image(window->mlx, WIDTH, HEIGHT);
-	window->addr = mlx_get_data_addr(window->img, 
-			&window->bits_per_pixel, &window->line_length, &window->endian);
+	window->addr = mlx_get_data_addr(window->img, &window->bits_per_pixel,
+			&window->line_length, &window->endian);
 	return (0);
 }
 
@@ -39,7 +39,7 @@ t_vec3	normalize(t_vec3 vec)
 	return (normalized);
 }
 
-t_vec3 vec_sub(t_vec3 a, t_vec3 b)
+t_vec3	vec_sub(t_vec3 a, t_vec3 b)
 {
 	t_vec3	sub;
 
@@ -49,7 +49,7 @@ t_vec3 vec_sub(t_vec3 a, t_vec3 b)
 	return (sub);
 }
 
-double vec_dot(t_vec3 a, t_vec3 b)
+double	vec_dot(t_vec3 a, t_vec3 b)
 {
 	return (a.x * b.x + a.y * b.y + a.z * b.z);
 }
@@ -63,7 +63,7 @@ int	intersect_sphere(t_vec3 camera, t_vec3 ray_dir, t_sphere sphere)
 	double	discriminant;
 	double	t1;
 	double	t2;
-	
+
 	oc = vec_sub(camera, sphere.center);
 	a = vec_dot(ray_dir, ray_dir);
 	b = 2 * vec_dot(oc, ray_dir);
@@ -76,21 +76,19 @@ int	intersect_sphere(t_vec3 camera, t_vec3 ray_dir, t_sphere sphere)
 	if (t1 > 0 || t2 > 0)
 		return (1);
 	return (0);
-	
 }
 
 int	ray_hit(int x, int y, t_vec3 camera, t_sphere sphere)
 {
-	int	image_x;
-	int	image_y;
-	int	focal_length;
+	int		image_x;
+	int		image_y;
+	int		focal_length;
 	t_vec3	ray;
 	t_vec3	ray_dir;
 
 	image_x = x - WIDTH / 2;
 	image_y = HEIGHT / 2 - y;
 	focal_length = 1000;
-
 	ray.x = image_x;
 	ray.y = image_y;
 	ray.z = focal_length;
@@ -100,11 +98,11 @@ int	ray_hit(int x, int y, t_vec3 camera, t_sphere sphere)
 
 void	fill_window(t_window *window)
 {
-	int	x;
-	int	y;
-	int	color;
-	int	pixel;
-	int hit;
+	int			x;
+	int			y;
+	int			color;
+	int			pixel;
+	int			hit;
 	t_sphere	sphere;
 	t_vec3		camera;
 
@@ -113,11 +111,9 @@ void	fill_window(t_window *window)
 	sphere.center.z = 10;
 	sphere.radius = 1;
 	sphere.color = 0x00FF0000;
-
 	camera.x = 0;
 	camera.y = 0;
 	camera.z = 0;
-
 	hit = 0;
 	y = 0;
 	while (y < HEIGHT)
@@ -130,7 +126,7 @@ void	fill_window(t_window *window)
 			else
 				color = 0x000000;
 			pixel = y * (window->line_length / 4) + x;
-			*((int *)window->addr + pixel) = color;	
+			*((int *)window->addr + pixel) = color;
 			x++;
 		}
 		y++;
@@ -146,9 +142,6 @@ int	main(void)
 	if (init_window(window))
 		return (1);
 	fill_window(window);
-	//mlx_key_hook(window->win, (void *)exit, NULL);
 	mlx_loop(window->mlx);
-	free(window->mlx);
-	free(window);
 	return (0);
 }
