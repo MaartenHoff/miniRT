cee6861413b183beb7c9819bc8c92ebd16b9e05b
adiler <ahmet.diler@hotmail.de>
2025-03-15 21:52:11 +0100

    intersect

--- source/vector_utils/hits.c
+++ source/vector_utils/hits.c
@@ -6,6 +6,7 @@ double	sphere_intersection(t_spheres *sphere, t_coords *origin, t_coords *d)
 	double		t;
 
 	oc = create_vector(origin, sphere->center);
-	t = 
+	t = -2 * dot_product(&oc, &d);
+	t = sqrt(pow(2 * dot_product(&oc, &d), 2) - 4 * dot_product(&oc, &oc) * ()
 	
 }
