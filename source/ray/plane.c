0c592997bd153c78357500f9d96a16a532a5ca4d
adiler <ahmet.diler@hotmail.de>
2025-03-19 14:13:46 +0100

    test files updated

--- source/ray/plane.c
+++ source/ray/plane.c
@@ -11,6 +11,8 @@ int	plane_hit(t_planes *plane, t_coords origin, t_coords direction, t_hit *hit)
 	hit->point = vec_add(origin, vec_mul(direction, t));
 	hit->distance = t;
 	hit->color = plane->color;
+	if (vec_skalar(direction, plane->vector) > 0)
+		plane->vector = vec_mul(plane->vector, -1);
 	hit->normal = plane->vector;
 	return (0);
 }
